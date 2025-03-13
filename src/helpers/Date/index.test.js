c9dd6576faf01390c66b8724cf07b0ca159ba744
Aharon-zerbib <amzyt770@gmail.com>
2025-03-13 18:05:58 +0100

    feat: -ajout de test pour les date (avec comentaire pour evaluateur )

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,24 @@
-/**
- * 
- */
+import { getMonth } from "./index";
 
+// Regroupe les test pour date
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+  describe("When getMonth is called", () => {
+    //vérifier getMonth reterne janvier
+    it("the function returns 'janvier' for 2022-01-01 as date", () => {
+      // Création correspondant a 1er janvier 2022
+      const date = new Date("2022-01-01");
+      // expect = affirmation  toEqual("janvier") que je matend bien a janvier
+      expect(getMonth(date)).toEqual("janvier");
     });
-})
 
+    // vérifier getMonth returne juillet
+    it("the function returns 'juillet' for 2022-07-08 as date", () => {
+      // Création correspondant a 8 juillet 2022
+      const date = new Date("2022-07-08");
+      // pareille que pour le premier test je sa doit etre une affirmation que je matend bien a juillet
+      expect(getMonth(date)).toEqual("juillet");
+    });
+  });
+});
+
+// du coup j;ai importer getMonth pour verifier si il retourne bien les mois normalement puisque dans la fonction getMonth je retourne MONTHS[date.getMonth()];
